4b340f7faa2e5e5cbff17a6eedf9c455dfb9e94e
alex <alex@alian>
2021-01-05 14:03:18 +0100

    Plus minus

--- eingabe.php
+++ eingabe.php
@@ -1,4 +1,6 @@
-
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,12 +11,18 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <title>Poor-Scampi</title>
+    <link rel="stylesheet" href="css/plusminus.css">
     <link rel="icon" href="Icon.png">
 </head>
+<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 <body style="overflow:hidden">
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<script src="js/PlusMinus.js"> </script>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
@@ -34,8 +42,14 @@
         </ul>
     </div>
 </nav>
+<body style="overflow:hidden;height: 100%; margin: 0;">
 
+<div id="wrapper" style="min-height:100%;background-color:#fbfbfb">
 <form action="rand.php" method="post">
+    <div class="xxx" data-name="quantity" data-value="2" data-id="80"></div>
+    <div class="xxx" data-name="quantity"></div>
+    <div class="xxx" data-name="quantity" data-value="100" data-id="99"></div>
+
     Zahl1: <input type="text" name="Zahl1"><br>
     Zahl2: <input type="text" name="Zahl2"><br>
     Zahl3: <input type="text" name="Zahl3"><br>
@@ -44,7 +58,7 @@
     Zahl6: <input type="text" name="Zahl6"><br>
     <input type="submit">
 </form>
-
+</div>
 
 </body>
 </html>
--- rand.php
+++ rand.php
@@ -12,9 +12,16 @@
     <link rel="icon" href="Icon.png">
 </head>
 <body style="overflow:hidden;height: 100%; margin: 0;">
+<script src="jquery.js"></script>
+<script>
+    $(function(){
+        $("#header").load("header.html");
+    });
+</script>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 <div id="wrapper" style="min-height:100%;background-color:#fbfbfb">
+    <div id="header"></div>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="home.html">
             <img src="Icon.png" width="30" height="30" alt="">
@@ -23,84 +30,41 @@
     </nav>
     <h3>Das sind unsere zahlen</h3>
     <?php
-    $random1 =  rand(1,49);
-    $random2 = rand(1,49);
-    $random3 = rand(1,49);
-    $random4 = rand(1,49);
-    $random5 = rand(1,49);
-    $random6 = rand(1,49);
-    $random = array($random1,$random2,$random3,$random4,$random5,$random6);
+    $random0 =  rand(1,49);
 
+    $random = array($random0);
+    $random0 =  rand(1,49);
     $arrlength = count($random);
-    for($x = 0; $x < $arrlength; $x++) {
-        while ($random[2] == $random[$x]){
-            $random[2] = rand(1,49);
+    for($y = 0; $y < 5; $y++){
+        for($x = 0; $x < $arrlength; $x++) {
+        while ($random0 == $random[$x]){
+            $random0 = rand(1,49);
         }}
+    $random[$x] = $random0;
+    $arrlength = count($random);
+    $random0 = rand(1,49);
+    }
+    sort($random);
 
     for($x = 0; $x < $arrlength; $x++) {
-        while ($random[3] == $random[$x]){
-            $random[3] = rand(1,49);
-        }}
-    for($x = 0; $x < $arrlength; $x++) {
-        while ($random[4] == $random[$x]){
-            $random[4] = rand(1,49);
-        }}
-    for($x = 0; $x < $arrlength; $x++) {
-        while ($random[5] == $random[$x]){
-            $random[5] = rand(1,49);
-        }}
-    for($x = 0; $x < $arrlength; $x++) {
-        while ($random[6] == $random[$x]){
-            $random[6] = rand(1,49);
-        }}
-
-
-    echo "<img src='Pictures/Kugel/zahlenkugel-v1-200px-".$random[1].".png' >";
-    echo "<img src='Pictures/Kugel/zahlenkugel-v1-200px-".$random2.".png' >";
-    echo "<img src='Pictures/Kugel/zahlenkugel-v1-200px-".$random3.".png' >";
-    echo "<img src='Pictures/Kugel/zahlenkugel-v1-200px-".$random4.".png' >";
-    echo "<img src='Pictures/Kugel/zahlenkugel-v1-200px-".$random5.".png' >";
-    echo "<img src='Pictures/Kugel/zahlenkugel-v1-200px-".$random6.".png' >";
-
+        echo "<img src='Pictures/Kugel/zahlenkugel-v1-200px-".$random[$x].".png' >";
+        }
     ?>
     <h3>Das sind deine zahlen</h3>
      <?php
      $i = 0;
 
-     $Zahl1 = $_POST["Zahl1"];
-     $Zahl2 = $_POST["Zahl2"];
-     $Zahl3 = $_POST["Zahl3"];
-     $Zahl4 = $_POST["Zahl4"];
-     $Zahl5 = $_POST["Zahl5"];
-     $Zahl6 = $_POST["Zahl6"];
-     echo "<img src='Pictures/Kugel/zahlenkugel-v1-200px-".$Zahl1.".png' >";
-     echo "<img src='Pictures/Kugel/zahlenkugel-v1-200px-".$Zahl2.".png' >";
-     echo "<img src='Pictures/Kugel/zahlenkugel-v1-200px-".$Zahl3.".png' >";
-     echo "<img src='Pictures/Kugel/zahlenkugel-v1-200px-".$Zahl4.".png' >";
-     echo "<img src='Pictures/Kugel/zahlenkugel-v1-200px-".$Zahl5.".png' >";
-     echo "<img src='Pictures/Kugel/zahlenkugel-v1-200px-".$Zahl6.".png' >";
+     $Zahl = array($_POST["Zahl1"],$_POST["Zahl2"],$_POST["Zahl3"],$_POST["Zahl4"],$_POST["Zahl5"],$_POST["Zahl5"]);
+     sort($Zahl);
 
      for($x = 0; $x < $arrlength; $x++) {
-         if ($Zahl1 == $random[$x]){$i++;}
-     }
-     for($x = 0; $x < $arrlength; $x++) {
-         if ($Zahl2 == $random[$x]){$i++;}
+         echo "<img src='Pictures/Kugel/zahlenkugel-v1-200px-".$Zahl[$x].".png' >";
      }
 
+     for($y = 0; $y < 5; $y++) {
      for($x = 0; $x < $arrlength; $x++) {
-         if ($Zahl3 == $random[$x]){$i++;}
+         if ($Zahl[$y] == $random[$x]){$i++;}
      }
-
-     for($x = 0; $x < $arrlength; $x++) {
-         if ($Zahl4 == $random[$x]){$i++;}
-     }
-
-     for($x = 0; $x < $arrlength; $x++) {
-         if ($Zahl5 == $random[$x]){$i++;}
-     }
-
-     for($x = 0; $x < $arrlength; $x++) {
-         if ($Zahl6 == $random[$x]){$i++;}
      }
 
      if ($i > 0){
