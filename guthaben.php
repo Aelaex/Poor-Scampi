e63b6776301c8499b57f25c8f55198b00bbf73b0
Vincent Knapp <vincent.knapp@web.de>
2021-01-06 11:05:22 +0100

    Start Login11

--- guthaben.php
+++ guthaben.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="en" style="height: 10%;
       margin: 0;">
@@ -12,6 +15,9 @@
     <link rel="icon" href="Icon.png">
 </head>
 <body style="overflow:hidden;height: 100%; margin: 0;">
+
+
+
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 <!--Header Loader-->
@@ -40,9 +46,15 @@
         </div>
 
         <div class="container">
-            <p align="center"> Email-Adresse: <input type="email"/></p>
-            <p align="center">Kennwort: <input type="password"/></p>
-            <button onclick="location.href='www.yoursite.com'">Click Me</button>
+            <?php
+                echo "<p> Guten Tag ".$_POST["mail"]." :)</p><br>";
+                echo "<p> Dein Aktuelles Guthaben beträgt";
+                echo "<p>".$_POST["mail"]."</p>";
+
+                echo "Favorite color is " . $_SESSION["value"] . ".<br>";
+
+
+            ?>
         </div>
 
         <div class="col-sm-auto">
